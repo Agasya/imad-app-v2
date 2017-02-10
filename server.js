a9faf65c5f4325f46aba6377234eff4185a60a65
Agasya <agasya499@gmail.com>
2017-02-10 12:45:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,21 +49,13 @@ function  createTemplate(data) {
     content = data.content;
     var htmlTemplate = `<html>
         <head>
-            <title>${title}</title>
+            <title>aaaaaaaaaaaaaaaaaaa</title>
             <meta  name="viewport" content="width=device-width, initial-scale=1" />
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
         <body>
             <div class="container">
-                <div><a href="/">Home</a></div>
-                <hr/>
-                <h3>${heading}</h3>
-                <div>
-                    ${date}
-                </div>
-                <div>
-                    ${content}
-                </div>
+                bbbbbbbbbbbbbbb
             </div>
         </body>
     </html>
