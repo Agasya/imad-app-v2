bc7c6e266d835bc24a4cd081277f054901ced8af
Agasya <agasya499@gmail.com>
2017-02-10 16:52:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
 app.get('/counter', function(req, res) {
-   res.send("AAAAAAA") ;
+   counter = counter + 1;
+   res.send(counter.toString()) ;
 });
 
 var articles = {
