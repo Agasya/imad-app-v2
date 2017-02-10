8841dfb7099aa403d51c7577b212138394238707
Agasya <agasya499@gmail.com>
2017-02-10 15:14:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var element = document.getEleemnt('madi');
+var element = document.getElement('madi');
 element.onclick = function () {
   element.style.marginLeft = '100px';
 };
