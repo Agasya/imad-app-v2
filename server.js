73ef15c743537d2a82adde4710fe0f2027a5a91c
Agasya <agasya499@gmail.com>
2017-02-10 12:42:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,70 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+var articles = {
+    'article-one': {
+        title: 'Article One | Agasya Rani',
+        heading: 'Article One',
+        date: 'Feb 10, 2016',
+        content: `<p>
+                    This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. 
+                </p>
+                <p>
+                    This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. 
+                </p>
+                <p>
+                    This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. This is content for Article-one. 
+                </p>`
+    },
+    'article-two': {
+        title: 'Article Two | Agasya Rani',
+        heading: 'Article Two',
+        date: 'Feb 10, 2016',
+        content: `<p>
+                    This is content for Article-Two. 
+                </p>`
+    },
+    'article-three': {
+        title: 'Article Three | Agasya Rani',
+        heading: 'Article Three',
+        date: 'Feb 10, 2016',
+        content: `<p>
+                    This is content for Article-Three. 
+                </p>`
+    },
+};
+
+function  createTemplate(data) {
+    title = data.title;
+    heading = data.heading;
+    date = data.date;
+    content = data.content;
+    var htmlTemplate = `<html>
+        <head>
+            <title>${title}</title>
+            <meta  name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div><a href="/">Home</a></div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+}
+
+app.get('/:articleName', function (req, res) {
+var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function (req, res) {
