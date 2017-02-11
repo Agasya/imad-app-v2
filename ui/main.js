944e441c2374f4b71026da790cfaafbe7f229f78
Agasya <agasya499@gmail.com>
2017-02-11 16:26:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,8 @@
-console.log('Loaded!');
-var element = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft+10;
-    element.style.marginLeft = marginLeft + 'px';
-}
+var button = document.getElementById("counter");
+var counter = 0;
 
-element.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+button.onclick = function() {
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
